e5eefcc4db7bbb36ee9e2fd3b3aa74a7c6a25293
Vennverse <shubhamdubexskd2001@gmail.com>
2022-02-22 16:09:00 +0000

    Updates

---
+++ precache-manifest.3abca6e759f6191e4acbfcc248ec74be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3ad0aacf36bc7c5f8fcef9dbce1eb10",
+    "url": "/Venn-Tiktok/index.html"
+  },
+  {
+    "revision": "3f3a58f5677173282a2a",
+    "url": "/Venn-Tiktok/static/css/2.7b4682b0.chunk.css"
+  },
+  {
+    "revision": "4ab487efccfa763c2dcf",
+    "url": "/Venn-Tiktok/static/css/main.d850c1a7.chunk.css"
+  },
+  {
+    "revision": "3f3a58f5677173282a2a",
+    "url": "/Venn-Tiktok/static/js/2.19510393.chunk.js"
+  },
+  {
+    "revision": "be7cff0a9e76d499bf24631a070ab61f",
+    "url": "/Venn-Tiktok/static/js/2.19510393.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ab487efccfa763c2dcf",
+    "url": "/Venn-Tiktok/static/js/main.e263c57d.chunk.js"
+  },
+  {
+    "revision": "d1fa31b08995f907858d",
+    "url": "/Venn-Tiktok/static/js/runtime-main.a188af6a.js"
+  }
+]);
